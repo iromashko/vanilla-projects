213a062de199e060821cd71e52f5219c337fd6ad
iromashko <iromashko@me.com>
2020-10-19 13:05:25 +0300

    styling links

---
+++ todo-app/script.js
@@ -0,0 +1,9 @@
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click', addTodo);
+
+function addTodo(event) {
+  event.preventDefault();
+}
