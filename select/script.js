4a25d64440676c455095db01c174caaa91369c46
iromashko <iromashko@me.com>
2020-10-28 01:06:40 +0300

    select complete

--- select/script.js
+++ select/script.js
@@ -2,6 +2,7 @@ import { Select } from '/select/select.js';
 
 const select = new Select('#select', {
   placeholder: 'Select',
+  selectedId: '2',
   data: [
     { id: '1', value: 'React' },
     { id: '2', value: 'Angular' },
@@ -10,6 +11,9 @@ const select = new Select('#select', {
     { id: '5', value: 'Next' },
     { id: '6', value: 'Nest' },
   ],
+  onSelect(item) {
+    console.log(`Selected item ${item}`);
+  },
 });
 
 window.s = select;
--- select/select.js
+++ select/select.js
@@ -1,15 +1,23 @@
-export const getTemplate = (data = [], placeholder) => {
-  const text = placeholder ?? 'Default placeholder';
+export const getTemplate = (data = [], placeholder, selectedId) => {
+  let text = placeholder ?? 'Default placeholder';
 
   const items = data.map((item) => {
+    let cls = '';
+    if (item.id === selectedId) {
+      text = item.value;
+      cls = 'selected';
+    }
     return `
-      <li class="select__item" data-type="item" data-value="${item.id}">${item.value}</li>
+      <li class="select__item ${cls}" data-type="item" data-id="${item.id}">${item.value}</li>
     `;
   });
 
   return `
+  <div class="select__backdrop" data-type="backdrop">
+  </div>
+  
   <div class="select__input" data-type="input">
-            <span>${text}</span>
+            <span data-type="value">${text}</span>
             <i class="fa fa-chevron-down" data-type="arrow"></i>
           </div>
           <div class="select__dropdown">
@@ -24,6 +32,7 @@ export class Select {
   constructor(selector, options) {
     this.$el = document.querySelector(selector);
     this.options = options;
+    this.selectedId = options.selectedId;
     this.#render();
     this.#setup();
   }
@@ -32,18 +41,26 @@ export class Select {
     const { placeholder, data } = this.options;
 
     this.$el.classList.add('select');
-    this.$el.innerHTML = getTemplate(data, placeholder);
+    this.$el.innerHTML = getTemplate(data, placeholder, this.selectedId);
   }
 
   #setup() {
     this.clickHandler = this.clickHandler.bind(this);
     this.$el.addEventListener('click', this.clickHandler);
     this.$arrow = this.$el.querySelector('[data-type="arrow"]');
+    this.$value = this.$el.querySelector('[data-type="value"]');
   }
 
   clickHandler(event) {
     const { type } = event.target.dataset;
-    if (type === 'input') this.toggle();
+    if (type === 'input') {
+      this.toggle();
+    } else if (type === 'item') {
+      const id = event.target.dataset.id;
+      this.select(id);
+    } else if (type === 'backdrop') {
+      this.close();
+    }
   }
 
   get isOpen() {
@@ -56,6 +73,7 @@ export class Select {
 
   destroy() {
     this.$el.removeEventListener('click', this.clickHandler);
+    this.$el.innerHTML = '';
   }
 
   open() {
@@ -67,5 +85,23 @@ export class Select {
     this.$el.classList.remove('open');
     this.$arrow.classList.remove('fa-chevron-up');
     this.$arrow.classList.add('fa-chevron-down');
+  }
+
+  get current() {
+    return this.options.data.find((item) => item.id === this.selectedId);
+  }
+
+  select(id) {
+    this.selectedId = id;
+    this.$value.textContent = this.current.value;
+
+    this.$el.querySelectorAll('[data-type="item"]').forEach((item) => {
+      item.classList.remove('selected');
+    });
+    this.$el.querySelector(`[data-id="${id}"]`).classList.add('selected');
+
+    this.options.onSelect ? this.options.onSelect(this.current) : null;
+
+    this.close();
   }
 }
