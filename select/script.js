b5e30bb858e6d179606df4c2f87ef6a0f65af667
iromashko <iromashko@me.com>
2020-10-27 22:07:43 +0300

    select part 2

--- select/script.js
+++ select/script.js
@@ -2,6 +2,4 @@ import { Select } from '/select/select.js';
 
 const select = new Select('#select', {});
 
-console.log(select);
-
 window.s = select;
--- select/select.js
+++ select/select.js
@@ -1,8 +1,8 @@
 export const getTemplate = () => {
   return `
-  <div class="select__input">
+  <div class="select__input" data-type="input">
             <span>Select</span>
-            <i class="fa fa-chevron-down"></i>
+            <i class="fa fa-chevron-down" data-type="arrow"></i>
           </div>
           <div class="select__dropdown">
             <ul class="select__list">
@@ -31,12 +31,46 @@ export const getTemplate = () => {
 export class Select {
   constructor(selector, options) {
     this.$el = document.querySelector(selector);
+    this.#render();
+    this.#setup();
+  }
+
+  #render() {
+    this.$el.classList.add('select');
+    this.$el.innerHTML = getTemplate();
+  }
+
+  #setup() {
+    this.clickHandler = this.clickHandler.bind(this);
+    this.$el.addEventListener('click', this.clickHandler);
+    this.$arrow = this.$el.querySelector('[data-type="arrow"]');
+  }
+
+  clickHandler(event) {
+    const { type } = event.target.dataset;
+    if (type === 'input') this.toggle();
+  }
+
+  get isOpen() {
+    return this.$el.classList.contains('open');
+  }
+
+  toggle() {
+    this.isOpen ? this.close() : this.open();
+  }
+
+  destroy() {
+    this.$el.removeEventListener('click', this.clickHandler);
   }
 
   open() {
     this.$el.classList.add('open');
+    this.$arrow.classList.remove('fa-chevron-down');
+    this.$arrow.classList.add('fa-chevron-up');
   }
   close() {
     this.$el.classList.remove('open');
+    this.$arrow.classList.remove('fa-chevron-up');
+    this.$arrow.classList.add('fa-chevron-down');
   }
 }
