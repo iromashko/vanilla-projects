cca8d45cdef78fb5bfa519ec3a69cfde788a0a4e
iromashko <iromashko@me.com>
2020-10-30 19:08:19 +0300

    code editor complete

--- code-editor/script.js
+++ code-editor/script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('textarea').forEach((el) =>
+document.querySelectorAll('textarea').forEach((el) => {
   el.addEventListener('keydown', function (e) {
     if (e.key === 'Tab') {
       e.preventDefault();
@@ -14,5 +14,23 @@ document.querySelectorAll('textarea').forEach((el) =>
       this.selectionStart = nextSelectionStart;
       this.selectionEnd = nextSelectionStart;
     }
-  })
-);
+  });
+  el.addEventListener('keyup', updateIframe);
+});
+
+function updateIframe() {
+  const mountedPoint = document.querySelector('[data-iframe]');
+  const html = document.querySelector('.square-html>textarea').value;
+  const css = document.querySelector('.square-css>textarea').value;
+  const javascript = document.querySelector('.square-javascript>textarea')
+    .value;
+
+  const page = `${html}<style>${css}</style><script>${javascript}</script>`;
+
+  const iframe = document.createElement('iframe');
+  iframe.src = 'data:text/html;charset=utf-8,' + page;
+  mountedPoint.innerHTML = '';
+  mountedPoint.append(iframe);
+}
+
+updateIframe();
