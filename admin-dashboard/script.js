c13f05521b771ef1c7f753d629119765a2a6a294
iromashko <iromashko@me.com>
2020-10-31 22:00:06 +0300

    admin dashboard complete

--- admin-dashboard/script.js
+++ admin-dashboard/script.js
@@ -55,3 +55,20 @@ var options = {
 
 var chart = new ApexCharts(document.querySelector('#apex1'), options);
 chart.render();
+
+let sidebarOpen = false;
+let sidebar = document.getElementById('sidebar');
+let sidebarCloseIcon = document.getElementById('sidebarIcon');
+
+function toggleSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add('sidebar_responsive');
+    sidebarOpen = true;
+  }
+}
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove('sidebar_responsive');
+    sidebarOpen = false;
+  }
+}
