fd6fb6c4e0ec5d0de1e68a0a3e7f0532caf945ae
iromashko <iromashko@me.com>
2020-11-01 03:57:11 +0300

    part 2

---
+++ virtual-keyboard/script.js
@@ -0,0 +1,101 @@
+const Keyboard = {
+  elements: {
+    main: null,
+    keysContainer: null,
+    keys: [],
+  },
+
+  eventHandlers: {
+    oninput: null,
+    onclose: null,
+  },
+
+  properties: {
+    value: '',
+    capsLock: false,
+  },
+
+  init() {
+    this.elements.main = document.createElement('div');
+    this.elements.keysContainer = document.createElement('div');
+
+    this.elements.main.classList.add('keyboard', '1keyboard--hidden');
+    this.elements.keysContainer.classList.add('keyboard__keys');
+
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
+  },
+  _createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '0',
+      'backspace',
+      'q',
+      'w',
+      'e',
+      'r',
+      't',
+      'y',
+      'u',
+      'i',
+      'o',
+      'p',
+      'caps',
+      'a',
+      's',
+      'd',
+      'f',
+      'g',
+      'h',
+      'j',
+      'k',
+      'l',
+      'enter',
+      'done',
+      'z',
+      'x',
+      'c',
+      'v',
+      'b',
+      'n',
+      'm',
+      ',',
+      '.',
+      '?',
+      'space',
+    ];
+    const createIconHTML = (icon_name) => {
+      return `<i class="material-icons">${icon_name}</i>`;
+    };
+    keyLayout.forEach((key) => {
+      const keyElement = document.createElement('button');
+      const insertLineBreak =
+        ['backspace', 'p', 'enter', '?'].indexOf(key) !== -1;
+    });
+  },
+  _triggerEvent(handlerName) {
+    console.log(`Handle Event: ` + handlerName);
+  },
+  _toggleCapsLock() {
+    console.log(`Caps lock toggled`);
+  },
+  open(initialValue, oninput, onclose) {
+    //
+  },
+  close() {
+    //
+  },
+};
+
+window.addEventListener('DOMContentLoaded', function () {
+  Keyboard.init();
+});
