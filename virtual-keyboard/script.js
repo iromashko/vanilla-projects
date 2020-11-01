d686a33c53013088616d8376d3accbfd83a270f0
iromashko <iromashko@me.com>
2020-11-01 14:49:09 +0300

    keyboard complete

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -19,12 +19,24 @@ const Keyboard = {
     this.elements.main = document.createElement('div');
     this.elements.keysContainer = document.createElement('div');
 
-    this.elements.main.classList.add('keyboard', '1keyboard--hidden');
+    this.elements.main.classList.add('keyboard', 'keyboard--hidden');
     this.elements.keysContainer.classList.add('keyboard__keys');
     this.elements.keysContainer.appendChild(this._createKeys());
 
+    this.elements.keys = this.elements.keysContainer.querySelectorAll(
+      '.keyboard__key'
+    );
+
     this.elements.main.appendChild(this.elements.keysContainer);
     document.body.appendChild(this.elements.main);
+
+    document.querySelectorAll('.use-keyboard-input').forEach((element) => {
+      element.addEventListener('focus', () => {
+        this.open(element.value, (currentValue) => {
+          element.value = currentValue;
+        });
+      });
+    });
   },
   _createKeys() {
     const fragment = document.createDocumentFragment();
@@ -174,16 +186,32 @@ const Keyboard = {
     return fragment;
   },
   _triggerEvent(handlerName) {
-    console.log(`Handle Event: ` + handlerName);
+    if (typeof this.eventHandlers[handlerName] === 'function') {
+      this.eventHandlers[handlerName](this.properties.value);
+    }
   },
   _toggleCapsLock() {
-    console.log(`Caps lock toggled`);
+    this.properties.capsLock = !this.properties.capsLock;
+
+    for (const key of this.elements.keys) {
+      if (key.childElementCount === 0) {
+        key.textContent = this.properties.capsLock
+          ? key.textContent.toUpperCase()
+          : key.textContent.toLowerCase();
+      }
+    }
   },
   open(initialValue, oninput, onclose) {
-    //
+    this.properties.value = initialValue || '';
+    this.eventHandlers.oninput = oninput;
+    this.eventHandlers.onclose = onclose;
+    this.elements.main.classList.remove('keyboard--hidden');
   },
   close() {
-    //
+    this.properties.value = '';
+    this.eventHandlers.oninput = oninput;
+    this.eventHandlers.onclose = onclose;
+    this.elements.main.classList.add('keyboard--hidden');
   },
 };
 
