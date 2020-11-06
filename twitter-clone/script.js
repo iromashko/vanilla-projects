46864838e5144af1d622f436e3f7a5dd344ec8a7
iromashko <iromashko@me.com>
2020-11-06 19:50:08 +0300

    part 6

--- twitter-clone/script.js
+++ twitter-clone/script.js
@@ -6,6 +6,13 @@ const newsFeedPage = document.querySelector('.feeds-page');
 const loginModal = document.querySelector('.login-modal');
 const modalX = document.querySelector('.login-modal i');
 const loginFormBtn = document.querySelector('.login-form-btn');
+const postBtn = document.querySelector('.post-btn');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modal = document.querySelector('.modal');
+const postModalX = document.querySelector('.modal-header i');
+const modalPostBtn = document.querySelector('.modal-header button');
+const modalFooterPlus = document.querySelector('.modal-footer span');
+const modalInput = document.querySelector('.modal-input');
 
 const goToLoginPage = () => {
   mainPage.style.display = 'none';
@@ -44,5 +51,37 @@ loginFormBtn.addEventListener('click', (e) => {
     newsFeedPage.style.display = 'block';
   } else {
     loginModal.style.display = 'block';
+  }
+});
+
+postBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+  modalWrapper.classList.add('modal-wrapper-display');
+});
+
+postModalX.addEventListener('click', () => {
+  modal.style.display = 'none';
+  modalWrapper.classList.remove('modal-wrapper-display');
+
+  if (modalInput.value !== '') {
+    modalInput.value = '';
+    changeOpacity(0.5);
+  }
+});
+
+const changeOpacity = (x) => {
+  modalPostBtn.style.opacity = x;
+  modalFooterPlus.style.opacity = x;
+};
+
+modalInput.addEventListener('keypress', (e) => {
+  if (e.target.value !== '') {
+    changeOpacity(1);
+  }
+});
+
+modalInput.addEventListener('blur', (e) => {
+  if (e.target.value === '') {
+    changeOpacity(0.5);
   }
 });
