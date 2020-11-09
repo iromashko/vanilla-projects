f5134d5d77368e797c3a44e875380028344e28dd
iromashko <iromashko@me.com>
2020-11-09 19:46:11 +0300

    electronic store complete

--- index-db/script.js
+++ index-db/script.js
@@ -14,6 +14,8 @@ const btnread = document.getElementById('btn-read');
 const btnupdate = document.getElementById('btn-update');
 const btndelete = document.getElementById('btn-delete');
 
+const notfound = document.getElementById('notfound');
+
 btncreate.onclick = () => {
   let flag = bulkcreate(db.products, {
     name: proname.value,
@@ -25,9 +27,47 @@ btncreate.onclick = () => {
   getData(db.products, (data) => {
     userid.value = data.id + 1 || 1;
   });
+
+  table();
+
+  let insertmsg = document.querySelector('.insertmsg');
+  getMsg(flag, insertmsg);
 };
 
 btnread.onclick = table;
+
+btnupdate.onclick = () => {
+  const id = parseInt(userid.value || 0);
+  if (id) {
+    db.products
+      .update(id, {
+        name: proname.value,
+        seller: seller.value,
+        price: price.value,
+      })
+      .then((updated) => {
+        let get = updated ? true : false;
+        let updatemsg = document.querySelector('.updatemsg');
+        getMsg(get, updatemsg);
+
+        proname.value = seller.value = price.value = '';
+      });
+  }
+};
+
+btndelete.onclick = () => {
+  db.delete();
+
+  db = productdb('Productdb', {
+    products: `++id,name,seller,price`,
+  });
+  db.open();
+  table();
+  textID(userid);
+
+  let deletemsg = document.querySelector('.deletemsg');
+  getMsg(true, deletemsg);
+};
 
 function table() {
   const tbody = document.getElementById('tbody');
@@ -56,15 +96,56 @@ function table() {
         createEl('td', tr, (td) => {
           createEl('i', td, (i) => {
             i.className += 'fas fa-trash-alt btn-delete';
+
+            i.setAttribute(`data-id`, data.id);
+            i.onclick = deletebtn;
           });
         });
       });
+    } else {
+      notfound.textContent = 'No record found in the database';
     }
   });
 }
 
 function editbtn(event) {
-  db.products.get(1, (data) => {
-    console.log(data);
+  console.log(event.target.dataset.id);
+  let id = parseInt(event.target.dataset.id);
+  db.products.get(id, (data) => {
+    userid.value = data.id || 0;
+    proname.value = data.name || '';
+    seller.value = data.seller || '';
+    price.value = data.price || '';
   });
+  table();
+}
+
+function deletebtn(event) {
+  let id = parseInt(event.target.dataset.id);
+  db.products.delete(id);
+  table();
+}
+
+window.onload = () => {
+  textID(userid);
+};
+
+function textID(textboxid) {
+  getData(db.products, (data) => {
+    textboxid.value = data.id + 1 || 1;
+  });
+}
+
+function getMsg(flag, element) {
+  if (flag) {
+    element.className += ' movedown';
+
+    setTimeout(() => {
+      element.classList.forEach((classname) => {
+        classname == 'movedown'
+          ? undefined
+          : element.classList.remove('movedown');
+      });
+    }, 4000);
+  }
 }
